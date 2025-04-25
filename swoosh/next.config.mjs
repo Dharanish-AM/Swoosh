e192b337306c8328aaff38eeb48b9c17242f9efc
Dharanish A M <dharanish816@gmail.com>
2025-04-25 22:05:59 +0530

    Update next.config.mjs

--- swoosh/next.config.mjs
+++ swoosh/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-};
-
-export default nextConfig;
+    experimental: {
+      turbo: {
+        enabled: false, 
+      },
+    },
+  };
+  
+  export default nextConfig;
